4ab64f4601e9cfc0f606b5acef92a89f28f7d13c
KarenJCastillo <kc6539769@gmail.com>
2026-02-10 13:31:27 -0500

    tutorial

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,25 @@
 package org.example;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.security.spec.RSAOtherPrimeInfo;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//karen juliana castillo barreto
-        System.out.println("hola karen ");
-        System.out.println("hola sasha");
-        System.out.println("hola maia");
+        //crear persona con constructor vacio
+    Persona p1 = new Persona();
+    p1.setNombre("cesilia");
+    p1.setEdad(90);
+    p1.setGenero("niña");
 
+    // crear persona con constructor lleno
+
+        Persona p2 = new Persona("sasha",3,"niña");
+
+        //imprimir contostring
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
 
 
     }
---
+++ src/main/java/org/example/Persona.java
@@ -0,0 +1,63 @@
+package org.example;
+
+public class Persona {
+    //atributos
+    private String nombre;
+    private int edad;
+    private String genero;
+
+
+    //contructores
+     //contructor vacio
+    public Persona() {
+    }
+     //contructor con_todo
+
+    public Persona(String nombre, int edad, String genero) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.genero = genero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", genero='" + genero + '\'' +
+                '}';
+    }
+
+
+    //metodos
+    public int calcularPasos(){
+       return 0 ;
+    }
+    public double calcularSalario (){
+        return 0.0;
+    }
+}
